c713188de733bcdf7072a64d592e8668fc88c365
Nikhil Dhiman <dhimannikhil905@gmail.com>
2025-11-16 17:39:10 +0530

    Updated login & signup UI layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,14 +24,14 @@ export default function Header() {
         {/* LEFT SIDE: LOGO + MENU */}
         <div className="flex items-center gap-x-12">
           {/* LOGO */}
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img alt="" src={logo} className="h-8 w-auto" />
           </a>
 
           {/* MENU NEXT TO LOGO */}
           <PopoverGroup className="hidden lg:flex lg:gap-x-10">
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            <a href="/" className="text-sm font-semibold text-gray-900">
               Home
             </a>
             <a href="#" className="text-sm font-semibold text-gray-900">
@@ -63,7 +63,7 @@ export default function Header() {
         <div className="hidden lg:flex lg:items-center lg:gap-x-6">
           {/* SIGN IN */}
           <a
-            href="#"
+            href="/login"
             className="flex items-center gap-2 text-sm font-semibold text-gray-900"
           >
             <svg
